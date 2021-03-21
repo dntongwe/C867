0b29a692ae6ccd19d518bbe706b1f0b367a313c6
Dieudonne <dmn55@cornell.edu>
2021-03-21 17:40:23 -0400

    almost complete

--- roster.h
+++ roster.h
@@ -13,21 +13,17 @@ class Roster
 		Roster();
 		~Roster();
 		Student* classRosterArray[5];
-		int rosterSize = 0;
 		void add(string studentID, string firstname, string lastname, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, string degreeProgram);
 		void remove(string studentID);
 		void printAll();
 		void printAverageDaysInCourse(string studentID);
 		void printInvalidEmails();
-		void printByDegreeProgram(string degreeProgram);
+		void printByDegreeProgram(Degree degreeProgram);
+		void printByDegreeProgram(string degreeProgram); // function overlop, search by string also
 		void setclassRosterArray();
-		int roster;
+		int roster;	
 
     private:
-		
-        
-
-
 };
 
 #endif
